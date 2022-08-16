009487f32c331f1267cd5f4feb95d8a3dec4548e
Sourabh Sinha <43882893+sourabhsinha396@users.noreply.github.com>
2022-08-16 12:26:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import os
-from typing import Optional
 from fastapi import FastAPI
-import uvicorn
-import random
+import requests
 
 app = FastAPI()
 
+
+@app.get("/{city}")
+def read_root(city:str):
+    try:
+        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=47ed18d2447f09799cb91500493d0692")
+        print(response.json())
+        return response.json().get('weather')[0]["main"]
+    except:
+        return {"message":"Please enter valid city name"}
+
+        
 @app.get("/")
-def home():
-    return {"Hello": "World from FastAPI"}
-
-# get random number between min(default:0) and max(default:9)
-@app.get("/random/")
-def get_random(min: Optional[int] = 0, max: Optional[int] = 9):
-    rval = random.randint(min, max)
-    return { "value": rval }
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", default=5000)), log_level="info")
+def read_index():
+    return {"message":"append any city name to the url after / e.g. www.url.com/pune"}
